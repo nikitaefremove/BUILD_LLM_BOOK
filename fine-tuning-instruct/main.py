6665f20c597ccf757c482e34efde69fd51324ab9
Nikita <nikitaefremove@yandex.ru>
2025-01-28 18:49:06 +0800

    feat: update main

--- fine-tuning-instruct/main.py
+++ fine-tuning-instruct/main.py
@@ -1,12 +1,10 @@
 import torch
-import tiktoken
 
 from utils.data_processing.format_input import format_input
 from utils.data_processing.partitioning_dataset import val_data
-from model.architecture import GPTModel
-from core.model_config import GPT_CONFIG_355M
 
 from utils.generate_text import generation_pipeline
+
 
 torch.manual_seed(123)
 input_text = format_input(val_data[0])
